3c9c5a7d558ef35a266d62c5b87edbc09bf3e355
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2022-03-17 11:14:45 -0600

    code before auth change

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,11 +2,14 @@ class Api::V1::UsersController < ApplicationController
     skip_before_action :authorized, only: [:create]
 
     def create 
+        puts "in users_controller_create"
         user = User.create(user_params)
         if user.valid?
+            puts "user is valid"
             token = encode_token(user_id: user.id)
             render json: {user: UserSerializer.new(user), jwt: token}, status: :created
         else
+            puts "user is not valid"
             render json: {error: "Could not create user!"}, status: :unprocessable_entity
         end
     end
